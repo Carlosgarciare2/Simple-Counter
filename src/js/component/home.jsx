8ac842265817c471356041649d8ff39319c920da
Carlosgarciare2 <122974615+Carlosgarciare2@users.noreply.github.com>
2023-05-05 19:31:26 +0000

    contador sin funcionalidad

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,23 +4,22 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+
+window.onload = () => {
+	const Home = () => {
+		return (
+			<div className="container d-flex bg-black text-white" id="caja">
+				<div className="a bg-dark fs-1 text m-3 p-2 rounded"><i class="fa-regular fa-clock"></i></div>
+				<div className="b bg-dark fs-1 text m-3 p-2 rounded">2</div>
+				<div className="c bg-dark fs-1 text m-3 p-2 rounded">3</div>
+				<div className="d bg-dark fs-1 text m-3 p-2 rounded">4</div>
+				<div className="e bg-dark fs-1 text m-3 p-2 rounded">5</div>
+				<div className="f bg-dark fs-1 text m-3 p-2 rounded">6</div>
+				<div className="g bg-dark fs-1 text m-3 p-2 rounded">7</div>
+			</div>
+		);
+	};
+}
+
 
 export default Home;
